2ee0f1f07eaea438bd9dd6670de647b54545a231
Tommy McGlynn <tmcglynn@apple.com>
2014-11-15 00:52:41 -0800

    eb configure

--- twowheels/settings/production.py
+++ twowheels/settings/production.py
@@ -1,6 +1,19 @@
 from .base import *
+import os
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],  # Set to empty string for localhost.
+        'PORT': os.environ['RDS_PORT'],  # Set to empty string for default.
+        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+    }
+}
 
 try:
 	from .local import *
