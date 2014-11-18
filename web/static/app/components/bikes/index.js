09519e11e7c97cf5446a49f5d0db525136d2e3e9
Doron Segal <doron2402@gmail.com>
2014-11-18 12:53:12 -0800

    adding client side

---
+++ web/static/app/components/bikes/index.js
@@ -0,0 +1,14 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  module.exports = {
+    Collection: require("./collection"),
+
+    Model: require("./model"),
+
+    Views: {
+      Item: require("./item/view"),
+      List: require("./list/view")
+    }
+  };
+});
---
+++ web/static/app/components/bikes/item/view.js
@@ -0,0 +1,25 @@
+define(function(require, exports, module) {
+  "use strict";
+
+  var app = require("app");
+
+  var Layout = Backbone.Layout.extend({
+    template: require("ldsh!./template"),
+
+    tagName: 'div class="col-xs-3 bike-item"',
+
+    serialize: function() {
+      return { bike: this.bike };
+    },
+
+    events: {
+
+    },
+
+    initialize: function() {
+      // this.listenTo(this.model, "change", this.render);
+    }
+  });
+
+  module.exports = Layout;
+});
