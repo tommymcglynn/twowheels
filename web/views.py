525dbe00ae8bb213f4b923a2b3fe50917f0a93c5
Tommy McGlynn <tmcglynn@apple.com>
2014-11-20 20:14:41 -0800

    Interface for gathering image data

--- web/views.py
+++ web/views.py
@@ -1,7 +1,57 @@
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
+from web.models import BikeStyle, BikeModel, BikePart
 
 def index(request):
     context = {}
     context['api_url'] = request.build_absolute_uri(reverse('api-root'))
     return render(request, 'home.html', context)
+
+def gather_images(request, partId=None, modelId=None, styleId=None, start='1'):
+    import urllib2
+    import json
+
+    start = int(start)
+    count = 8
+    dataInfo = None
+    searchTerm = ''
+    styles = []
+    models = []
+    parts = []
+    part = None
+    model = None
+    style = None
+    if partId:
+        part = BikePart.objects.get(id=partId)
+    if modelId:
+        model = BikeModel.objects.get(id=modelId)
+    if styleId:
+        style = BikeStyle.objects.get(id=styleId)
+    if partId and modelId and styleId:
+        searchTerm = '%s+%s+%s' % (part.name, model.name, style.name)
+        searchTerm = searchTerm.replace(' ','+').replace('&', '+')
+        # Notice that the start changes for each iteration in order to request a new set of images for each loop
+        url = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=%s&rsz=%d&start=%d' % (searchTerm, count, start)
+
+        apiRequest = urllib2.Request(url)
+        response = urllib2.urlopen(apiRequest)
+        # Get results using JSON
+        results = json.load(response)
+        data = results['responseData']
+        dataInfo = data['results']
+    elif partId and modelId:
+        # load styles
+        styles = BikeStyle.objects.all()
+    elif partId:
+        # load models
+        models = BikeModel.objects.all()
+    else:
+        # load parts
+        parts = BikePart.objects.all()
+
+    # Iterate for each result and get unescaped url
+    context = {'results': dataInfo, 'searchTerm': searchTerm, 'nextStart': start+count,
+               'partId': partId, 'modelId': modelId, 'styleId': styleId,
+               'part': part, 'model': model, 'style': style,
+               'styles': styles, 'models': models, 'parts': parts}
+    return render(request, 'image_gather.html', context)
